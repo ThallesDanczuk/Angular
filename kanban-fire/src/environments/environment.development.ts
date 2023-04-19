ddb8579281cceb5a89e47425b039c29b4b3a5a36
ThallesDanczuk <thallesdanczuk33@gmail.com>
2023-04-19 16:27:26 -0300

    first commit

---
+++ kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-e5fd2',
+    appId: '1:615217391686:web:290290a6a893fb274c0247',
+    storageBucket: 'kanbanfire-e5fd2.appspot.com',
+    apiKey: 'AIzaSyDTimIWc66IxixiZupw867fN5IwDA8W-UI',
+    authDomain: 'kanbanfire-e5fd2.firebaseapp.com',
+    messagingSenderId: '615217391686',
+    measurementId: 'G-0WSSQN2BME',
+  },};
---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,22 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
+};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDTimIWc66IxixiZupw867fN5IwDA8W-UI",
+    authDomain: "kanbanfire-e5fd2.firebaseapp.com",
+    projectId: "kanbanfire-e5fd2",
+    storageBucket: "kanbanfire-e5fd2.appspot.com",
+    messagingSenderId: "615217391686",
+    appId: "1:615217391686:web:290290a6a893fb274c0247",
+    measurementId: "G-0WSSQN2BME"
+  };
+
